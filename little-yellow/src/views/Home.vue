6ae74f199547c340f9a2b906fccd094dcc7fbd95
unknown <xuly@zhchuangsi.com>
2021-06-01 17:58:36 +0800

    feat:基本页面布局构建

---
+++ little-yellow/src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <index v-show="active === 0"></index>
+      <message v-show="active === 2"></message>
+      <my v-show="active === 3"></my>
+    </div>
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="search">待定</van-tabbar-item>
+      <van-tabbar-item icon="friends-o">消息</van-tabbar-item>
+      <van-tabbar-item icon="setting-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+import index from "./index"
+import message from "./message"
+import my from "./my"
+
+export default {
+  name: "Home",
+  data(){
+    return {
+      active: 0
+    }
+  },
+  components: {
+    index,
+    message,
+    my
+  },
+};
+</script>
+<style lang="less" scoped>
+.home {
+  width: 100%;
+  .container{
+    width: 100%;
+  }
+}
+</style>
---
+++ little-yellow/src/views/index/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <div class="header-container">
+      <van-row>
+        <van-col span="4">
+          <div>
+            <van-icon name="plus" />
+          </div>
+        </van-col>
+        <van-col span="16">
+          <van-tabs v-model="active" class="header-center-item">
+            <van-tab title="推荐">
+                
+            </van-tab>
+            <van-tab title="关注">
+
+            </van-tab>
+            <van-tab title="本地">
+
+            </van-tab>
+          </van-tabs>
+        </van-col>
+        <van-col span="4">
+          <div>
+            <van-icon name="search" />
+          </div>
+        </van-col>
+      </van-row>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      active: 0,
+    };
+  },
+};
+</script>
+<style lang="less" scoped>
+.container {
+  width: 100%;
+  .header-container {
+      margin-top: 5px;
+    width: 100%;
+    height: 50px;
+    line-height: 50px;
+    // background: pink;
+    .header-center-item {
+        height: 50px;
+    }
+  }
+}
+</style>
